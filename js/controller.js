577ef024c61835808ab153d878d5977f20690ef8
jayaninadee <jayaninadeeshaa>
2019-07-12 22:12:14 +0530

    14

--- js/controller.js
+++ js/controller.js
@@ -2,4 +2,11 @@ $(function () {
 var container   =   $('#container');
 var rocket      =   $('#rocket');
 var other1      =   $('other1');
+
+var containerLeft    =   parseInt(container.css('left'));
+var containerWidth   =   parseInt(container.width());
+var containerHeight  =   parseInt(container.height());
+var rocketWidth      =   parseInt(rocket.width());
+var rocketHeight     =   parseInt(rocket.height());
+
 });
