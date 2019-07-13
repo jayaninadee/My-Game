63a873c4319d7095fba0566ee2842d69a2b772a1
jayaninadee <jayaninadeeshaa>
2019-07-13 13:53:38 +0530

    20

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,11 @@
 $(function () {
+    var animationID;
 var container        =   $('#container');
 var rocket           =   $('#rocket');
 var other1           =   $('other1');
+var other2           =   $('other2');
+var other3           =   $('other3');
+
 
 var containerLeft    =   parseInt(container.css('left'));
 var containerWidth   =   parseInt(container.width());
@@ -53,7 +57,54 @@ $(document).on('onkeydown',function (e) {
         }
     });
 
+    function left() {
+    if (gameOver===false && parseInt(rocket.css('left'))>0){
+    rocket.css('left',parseInt(rocket.css('left'))-5);
+    moveLeft =requestAnimationFrame(left);
+        }
+    }
+    function right() {
+        if (gameOver===false && parseInt(rocket.css('left'))<containerWidth-rocketWidth){
+            rocket.css('left',parseInt(rocket.css('left'))+5);
+            moveRight =requestAnimationFrame(right);
+        }
+    }
+    function up() {
+        if (gameOver===false && parseInt(rocket.css('top'))>0){
+            rocket.css('top',parseInt(rocket.css('top'))-3);
+            moveUp =requestAnimationFrame(up);
+        }
+    }
+    function down() {
+        if (gameOver===false && parseInt(rocket.css('top'))<containerHeight-rocketHeight){
+            rocket.css('top',parseInt(rocket.css('top'))+3);
+            moveUp =requestAnimationFrame(down);
+        }
+    }
 
+    animationID =requestAnimationFrame(repeat);
+    function repeat() {
+        if (collision(rocket,other1) || collision(rocket,other2),collision(rocket,other3));
+    }
+
+
+    function collision($div1, $div2) {
+        var x1 = $div1.offset().left;
+        var y1 = $div1.offset().top;
+        var h1 = $div1.outerHeight(true);
+        var w1 = $div1.outerWidth(true);
+        var b1 = y1 + h1;
+        var r1 = x1 + w1;
+        var x2 = $div2.offset().left;
+        var y2 = $div2.offset().top;
+        var h2 = $div2.outerHeight(true);
+        var w2 = $div2.outerWidth(true);
+        var b2 = y2 + h2;
+        var r2 = x2 + w2;
+
+        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+        return true;
+    }
 
 });
 
